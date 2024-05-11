c7251eacb292abc13bb81b74531b7a5cb9aeee77
Abhishek Kumar <Abhishekk55@birlasoft.com>
2024-05-11 19:30:36 +0530

    feat: initial project structure

--- src/main/java/com/boot/application/Application.java
+++ src/main/java/com/boot/application/Application.java
@@ -2,24 +2,13 @@ package com.boot.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
-import com.boot.application.dao.CustomerRepository;
-import com.boot.application.entities.Customer;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-	    ApplicationContext context = SpringApplication.run(Application.class, args);
-	    
-	    CustomerRepository customerRepo = context.getBean(CustomerRepository.class);
-	    
-	    Customer c1 = new Customer();
-	    c1.setCustomerName("John Doe");
-	    
-	    Customer result = customerRepo.save(c1);
-	    System.out.println(result.getCustomerName());
+	    SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/main/java/com/boot/application/dao/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.boot.application.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.boot.application.entities.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer>{
-
-}
